f23cfb407cf80243f990088c48a762dc24f8e5e8
Satyam Singh <businesswithsatyam9555@gmail.com>
2024-04-23 16:14:39 +0530

    cors update

--- backend/prod.js
+++ backend/prod.js
@@ -13,7 +13,17 @@ dotenv.config();
 const app = express();
 
 // Enable CORS
-app.use(cors());
+const allowedOrigins = ['https://medics-rosy.vercel.app/'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
 app.use(express.json());
 
 // Define storage for uploaded images using multer
